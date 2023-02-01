6491b8b43b2536a1ea591c001b60fa558414a877
badsyn <badsyn@oaciq.com>
2023-02-01 09:36:55 -0500

    Update nestable.rb

--- lib/rails_admin_nestable/nestable.rb
+++ lib/rails_admin_nestable/nestable.rb
@@ -13,11 +13,13 @@ module RailsAdmin
         end
 
         register_instance_option :collection? do
-          true
+          #true
+          false
         end
 
         register_instance_option :member? do
-          false
+          #false
+          true
         end
 
         register_instance_option :controller do
@@ -31,8 +33,11 @@ module RailsAdmin
 
             # Methods
             def update_tree(tree_nodes, parent_node = nil)
+              owner =  @abstract_model.model.find(params[:id].to_i)
+              
               tree_nodes.each do |key, value|
-                model = @abstract_model.model.find(value['id'].to_s)
+                #model = @abstract_model.model.find(value['id'].to_s)
+                model = owner.send(@nestable_conf.options[:children_field]).find(value['id'].to_i)
                 model.parent = parent_node || nil
                 model.send("#{@position_field}=".to_sym, (key.to_i + 1)) if @position_field.present?
                 model.save!(validate: @enable_callback)
@@ -42,7 +47,9 @@ module RailsAdmin
 
             def update_list(model_list)
               model_list.each do |key, value|
-                model = @abstract_model.model.find(value['id'].to_s)
+                #model = @abstract_model.model.find(value['id'].to_s)
+                owner = @abstract_model.model.find(params['id'].to_i)
+                model = owner.send(@nestable_conf.options[:children_field]).find(value['id'].to_i)
                 model.send("#{@position_field}=".to_sym, (key.to_i + 1))
                 model.save!(validate: @enable_callback)
               end
@@ -67,25 +74,31 @@ module RailsAdmin
             end
 
             if request.get?
-              query = list_entries(@model_config, :nestable, false, false).reorder(nil)
+              #query = list_entries(@model_config, :nestable, false, false).reorder(nil)
 
-              case @options[:scope].class.to_s
-                when 'Proc'
-                  query.merge!(@options[:scope].call(params))
-                when 'Symbol'
-                  query.merge!(@abstract_model.model.public_send(@options[:scope]))
-              end
+              # case @options[:scope].class.to_s
+              #  when 'Proc'
+              #    query.merge!(@options[:scope].call(params))
+              #  when 'Symbol'
+              #    query.merge!(@abstract_model.model.public_send(@options[:scope]))
+              # end
+
+              model = @abstract_model.model.find(params['id'].to_i)
+              children_field = @nestable_conf.options[:children_field]
 
               if @nestable_conf.tree?
                 @tree_nodes = if @options[:position_field].present?
-                  query.arrange(order: @options[:position_field])
+                   #query.arrange(order: @options[:position_field])
+                   model.send(children_field).arrange(order: @nestable_conf.options[:position_field])
                 else
-                  query.arrange
+                    #query.arrange
+                    model.send(children_field).arrange
                 end
               end
 
               if @nestable_conf.list?
-                @tree_nodes = query.order("#{@options[:position_field]} ASC")
+                #@tree_nodes = query.order("#{@options[:position_field]} ASC")
+                @tree_nodes = model.send(children_field).order(@nestable_conf.options[:position_field])
               end
 
               render action: @action.template_name
